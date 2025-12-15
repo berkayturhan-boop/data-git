98a5cb76d619142604e94b15f95ec86c2222152c
berkayturhan <r.berkayturhan@gmail.com>
2025-12-15 17:30:36 +0300

    Stil hatalari giderildi ve docstring eklendi

--- today.py
+++ today.py
@@ -1,5 +1,16 @@
-'''
-This is the module docstring
-'''
+"""
+Bu modül, challenge kapsamında isim döndüren fonksiyonları içerir.
+"""
+
 def my_name_is():
+    """
+    Benim GitHub kullanıcı adımı döndüren fonksiyon.
+    """
     return "berkayturhan-boop"
+
+
+def my_buddy_is():
+    """
+    Arkadaşımın ismini döndüren fonksiyon.
+    """
+    return "tmy-datamesa"
